511e9b15d7c7edda9b304f4f7aaa2da70147071e
Aram9988 <145191963+Aram9988@users.noreply.github.com>
2026-09-06 02:54:10 +0300

    Replace old entry route with new command center

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,6 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-import { useAppStore } from "@/lib/store";
-import { getUser } from "@/services/userService";
-import { firstAllowedRoute } from "@/lib/nav";
+import { createFileRoute } from "@tanstack/react-router";
+import CommandCenterApp from "@/v2/App";
 
 export const Route = createFileRoute("/")({
-  beforeLoad: () => {
-    const uid = useAppStore.getState().currentUserId;
-    const user = getUser(uid);
-    throw redirect({ to: firstAllowedRoute(user) });
-  },
+  component: CommandCenterApp,
 });
