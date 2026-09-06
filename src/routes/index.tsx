d7c565bb4457eed19841861e20c72a81cdf1ef1a
Aram9988 <145191963+Aram9988@users.noreply.github.com>
2026-09-07 00:02:25 +0300

    Activate live organizational command tree and editable roles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
-import OperationalCenter from "@/v7/App";
+import OrganizationalCommandCenter from "@/v8/App";
 
 export const Route = createFileRoute("/")({
-  component: OperationalCenter,
+  component: OrganizationalCommandCenter,
 });
