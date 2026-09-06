238a3ed5e8062dc6c61f97257dffd602d9c39173
Aram9988 <145191963+Aram9988@users.noreply.github.com>
2026-09-06 03:38:51 +0300

    Switch application to projects and tasks center

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
-import CommandCenterV4 from "@/v4/App";
+import ProjectsTasksCenter from "@/v5/App";
 
 export const Route = createFileRoute("/")({
-  component: CommandCenterV4,
+  component: ProjectsTasksCenter,
 });
